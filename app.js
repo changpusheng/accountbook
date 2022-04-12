9ba99334d97f6ae00c5294add538b97ded107135
pusheng <sdf753741789@gmail.com>
2022-04-12 22:36:41 +0800

    fix code

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.engine('hbs', exphbs({
 app.set('view engine', 'hbs')
 
 app.use(session({
-  secret: SESSION_SECRET,
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
